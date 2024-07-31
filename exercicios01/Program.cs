aaa96a6321e7a7f7eebd6ceafb4691119365b6e3
Matheus Maia <matheuscleciomaia@hotmail.com>
2024-07-30 23:12:06 -0300

    Exercicio 7

--- exercicios01/Program.cs
+++ exercicios01/Program.cs
@@ -7,47 +7,55 @@ using System.Security.Cryptography.X509Certificates;
 using System.Xml.Linq;
 internal class Program
 {
-    // Numero 6
+    // Numero 7
 
-    //Fazer um programa para ler um conjunto de nomes de pessoas e suas respectivas idades. Depois, mostrar na tela o nome
-    //da pessoa mais velha.
-    // Entrada: 5
-    //Joao 16
-    //Maria 21
-    //Teresa 15
-    //Carlos 23
-    //Paulo 17
+    //Fazer um programa para ler um conjunto de N nomes de alunos, bem como as notas que eles tiraram no 1º e 2º semestres. 
+    //Cada uma dessas informações deve ser armazenada em um vetor.Depois, imprimir os nomes dos alunos aprovados,
+    //considerando aprovados aqueles cuja média das notas seja maior ou igual a seis.
 
-    // Saída: Pessoa mais velha: Carlos
+    //Entrada:  4
+    //Joao  7.0  8.5 
+    //Maria  9.2  6.5 
+    //Carlos  5.0  6.0 
+    //Teresa  5.5  6.5 
+
+    // Saída
+    //Alunos aprovados: 
+    //Joao
+    //Maria
+    //Teresa
 
     private static void Main(string[] args)
     {
-        int N, minimo = int.MinValue;
-        int[] idade;
-        string[] nome, vet;
-        string velho="sem nome";
+        int N;
+        double[] nota1, nota2, media;
+        string[] vet, nome;
 
         N = int.Parse(Console.ReadLine());
-        idade = new int[N];
+        nota1 = new double[N];
+        nota2 = new double[N];
+        media = new double[N];
         nome = new string[N];
 
-        for (int i = 0; i<N;i++)
+        
+        for (int i = 0; i < N; i++)
         {
             vet = Console.ReadLine().Split(' ');
             nome[i] = vet[0];
-            idade[i] = int.Parse(vet[1]);
+            nota1[i] = double.Parse(vet[1], CultureInfo.InvariantCulture);
+            nota2[i] = double.Parse(vet[2], CultureInfo.InvariantCulture);
+            media[i] = (nota1[i] + nota2[i])/2;
             
         }
-        for (int i = 0; i < N; i++)
+
+        Console.WriteLine("Alunos aprovados:");
+        for (int i = 0; i < N; i++) 
         {
-            if (idade[i] > minimo)
+           if (media[i] >= 6)
             {
-                minimo = idade[i];
-                velho = nome[i];
+                Console.WriteLine(nome[i]);
             }
         }
-
-        Console.WriteLine("Pessoa mais velha: " + velho);
 
 
         Console.ReadKey();
