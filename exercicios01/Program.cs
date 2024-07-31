aef0b4b60219a5b47dfba560eac7f768592d49f8
Matheus Maia <matheuscleciomaia@hotmail.com>
2024-07-30 21:49:57 -0300

    Exercicio 4 resolvido

--- exercicios01/Program.cs
+++ exercicios01/Program.cs
@@ -1,49 +1,61 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.Globalization;
 using System.Net;
+using System.Net.Http.Headers;
 using System.Runtime.ConstrainedExecution;
 using System.Security.Cryptography.X509Certificates;
 using System.Xml.Linq;
 internal class Program
 {
-    //Faça um programa para ler dois vetores A e B, contendo N elementos cada.Em seguida, gere um terceiro vetor C onde
-    //cada elemento de C é a soma dos elementos correspondentes de A e B.Imprima o vetor C gerado.
+    // Numero 4
+
+    //Fazer um programa para ler um vetor de N números reais. Em seguida,
+    //- Mostrar na tela a média aritmética de todos elementos
+    //- Depois mostrar todos os elementos do vetor que estejam abaixo da média.
+
     //Entrada
-    //6
-    //8 2 11 14 13 20
-    //5 10 3 1 10 7
+    //4
+    //10.0 15.5 13.2 9.8
 
     //Saída
-    //13 12 14 15 23 27
+    //12.125
+    //10.0
+    //9.8
 
 
     private static void Main(string[] args)
     {
         int N;
         string[] vet;
-        int[] valor1, valor2, resultado;
+        double[] elementos;
+        double media = 0.0;
 
         N = int.Parse(Console.ReadLine());
-        valor1 = new int[N];
-        valor2 = new int[N];
-        resultado = new int[N];
+        elementos = new double[N];
         vet = Console.ReadLine().Split(' ');
 
-        for (int i = 0; i < N; i++)
+        for (int i = 0; i < N; i++) 
         {
-            valor1[i] = int.Parse(vet[i]);
+            elementos[i] = double.Parse(vet[i], CultureInfo.InvariantCulture);
+            media = media + elementos[i];
         }
-        vet = Console.ReadLine().Split(' ');
+        media = media / N;
+        Console.WriteLine(media);
+        
+        for (int i = 0; i< N; i++)
+        {
+            if (elementos[i] < media)
+            {
+                Console.WriteLine(elementos[i].ToString("F1"), CultureInfo.InvariantCulture);
+            }
+        }
 
-        for (int i = 0; i < N; i++)
-        {
-            valor2[i] = int.Parse(vet[i]);
-        }
-        for (int i = 0; i < N; i++)
-        {
-            resultado[i] = valor1[i] + valor2[i];
-            Console.Write(resultado[i]+ " ");
-        }
+
+
+
+
+
+
 
         Console.ReadKey();
     }
