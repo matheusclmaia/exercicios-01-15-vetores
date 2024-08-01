a9e7f8eac6c11a1a24a31c3cad85b69ea1a9cf5d
Matheus Maia <matheuscleciomaia@hotmail.com>
2024-08-01 00:30:39 -0300

    Exercicio 9

--- exercicios01/Program.cs
+++ exercicios01/Program.cs
@@ -1,85 +1,109 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Drawing;
 using System.Globalization;
 using System.Net;
 using System.Net.Http.Headers;
 using System.Runtime.ConstrainedExecution;
 using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
+using System.Text;
 using System.Xml.Linq;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 internal class Program
 {
-    // Numero 8
+    // Numero 9
 
-    //Tem-se um conjunto de dados contendo a altura e o sexo (M, F) de N pessoas. Fazer um programa que calcule e escreva:
-    // a maior e a menor altura do grupo
-    // a média de altura das mulheres
-    // o número de homens
+    //Um comerciante deseja fazer o levantamento do lucro das mercadorias que ele comercializa. Para isto, mandou digitar 
+    //um conjunto de N mercadorias, cada uma contendo nome, preço de compra e preço de venda das mesmas.Fazer um
+    //programa que leia tais dados e determine e escreva quantas mercadorias proporcionaram: 
+    // lucro< 10% 
+    // 10% ≤ lucro ≤ 20% 
+    // lucro> 20% 
+    //Determine e escreva também o valor total de compra e de venda de todas as mercadorias, assim como o lucro total.
 
-    //Entrada:  5
-    //1.70 F
-    //1.83 M
-    //1.54 M
-    //1.61 F
-    //1.75 F
+    //Entrada 4
+    // Feijao  10.00  11.00 
+    // Arroz   12.00  12.80 
+    // Oleo     5.00  5.70 
+    // Sal      3.00 4.00 
 
-    // Saída
-    //Menor altura = 1.54
-    //Maior altura = 1.83
-    //Media das alturas das mulheres = 1.69
-    //Numero de homens = 2
+    //Saída:
+    //Lucro abaixo de 10%: 1 
+    //Lucro entre 10% e 20%: 2 
+    //Lucro acima de 20%: 1 
+    //Valor total de compra: 30.00 
+    //Valor total de venda: 33.50 
+    //Lucro total: 3.50 
 
 
     private static void Main(string[] args)
     {
-        int N, homens=0, mulher=0;
-        double menor = double.MaxValue , maior = double.MinValue, mediamulheres=0;
-        double[] altura;
-        string[] sexo, vet;
-        
-      
+        int N, contador = 0;
+        double[] compra, venda, lucro;
+        string[] vet, produto;
+        double totalcompra =0 , totalvenda=0;
+
+
         N = int.Parse(Console.ReadLine());
 
-        altura = new double[N];
-        sexo   = new string[N];
+        compra  = new double[N];
+        venda   = new double[N];
+        produto = new string[N];
+        lucro   = new double[N];
+        
 
         for (int i = 0; i < N; i++)
         {
             vet = Console.ReadLine().Split(' ');
-            altura[i] = double.Parse(vet[0], CultureInfo.InvariantCulture);
-            sexo[i] = vet[1];
+            produto[i] = vet[0];
+            compra[i] = double.Parse(vet[1], CultureInfo.InvariantCulture);
+            venda[i] = double.Parse(vet[2], CultureInfo.InvariantCulture);
+            lucro[i] = venda[i] - compra[i]  ;
+            totalcompra += compra[i];
+            totalvenda  += venda[i];
         }
-        for (int i =0; i < N ; i++)
-        { 
 
-            //Menor altura
-            if (altura[i] < menor)
+        // Lucro abaixo de 10%
+        for (int i = 0; i < N; i++)
+        {
+
+            if (lucro[i] < compra[i] * (0.1))
             {
-                menor = altura[i];
-            }
-            //Maior altura
-            if (altura[i] > maior)
-            {
-                maior = altura[i];
-            }
-            // Altura media das mulheres
-            if (sexo[i] == "F")
-            {
-                mediamulheres += altura[i];
-                mulher++;
-            }
-            // Quantidade de homens
-            if (sexo[i] == "M")
-            {
-                homens++;
+                contador++;
             }
         }
-        mediamulheres = mediamulheres / mulher;
+        Console.WriteLine("\nLucro abaixo de 10 %: " + contador);
+        contador = 0;
 
-        Console.WriteLine("Menos altura = " + menor.ToString("F2",CultureInfo.InvariantCulture));
-        Console.WriteLine("Maior altura = " + maior.ToString("F2", CultureInfo.InvariantCulture));
-        Console.WriteLine("Media das alturas das Mulheres = " + mediamulheres.ToString("F2", CultureInfo.InvariantCulture));
-        Console.WriteLine("Numero de Homens = " + homens);
+        //Lucro entre 10% e 20%: 
+        for (int i = 0; i < N; i++)
+        {
+            if (lucro[i] >= compra[i] * (0.1) && lucro[i] <= compra[i] * (0.2))
+            {
+                contador ++;
+            }
+        }
+        Console.WriteLine("Lucro entre 10% e 20%: " + contador);
+        contador = 0;
+
+        //Lucro acima de 20%: 
+        for (int i = 0; i < N; i++)
+        {
+            if (lucro[i] > compra[i] * (0.2))
+            {
+                contador++;
+            }
+        }
+        Console.WriteLine("Lucro acima de 20%: " + contador);
+        contador = 0;
+
+        //Valor total de compra: 30.00 
+        //Valor total de venda: 33.50 
+        //Lucro total: 3.50 
+        Console.WriteLine("Valor total de compra: " + totalcompra.ToString("F2",CultureInfo.InvariantCulture));
+        Console.WriteLine("Valor total de venda: " + totalvenda.ToString("F2", CultureInfo.InvariantCulture));
+        totalcompra = totalvenda - totalcompra;
+        Console.WriteLine("Lucro total: " + totalcompra.ToString("F2", CultureInfo.InvariantCulture));
 
 
         Console.ReadKey();
